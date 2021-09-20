566f23b014874ac6b0cbe047adb91f6dd1e9d726
romanich-kar <81390143+romanich-kar@users.noreply.github.com>
2021-09-20 19:14:15 +0300

    Домашнее задание к уроку №5
    II вариант
    1) Создать пауков по сбору данных о книгах с сайтов labirint.ru и/или book24.ru
    2) Каждый паук должен собирать:
    * Ссылку на книгу
    * Наименование книги
    * Автор(ы)
    * Основную цену
    * Цену со скидкой
    * Рейтинг книги
    3) Собранная информация должна складываться в базу данных

---
+++ labirint.py
@@ -0,0 +1,30 @@
+import scrapy
+from scrapy.http import HtmlResponse
+from pymongo import MongoClient
+
+client = MongoClient(host='localhost', port=27017)
+db = client.books
+
+class LabirintSpider(scrapy.Spider):
+    name = 'labirint'
+    allowed_domains = ['https://www.labirint.ru']
+    start_urls = ['http://https://www.labirint.ru/books/']
+    pages_count = 50
+
+    def start_requests(self):
+        for page in range(1, 1 + self.pages_count):
+            url = f'https://www.labirint.ru/books/{page}/'
+            yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response, **kwargs):
+        item = {
+            'url': response.request.url,
+            'title': response.css('#product-title::text').extract_first().strip(),
+            'author': response.css('div.authors a::text').extract_first().strip(),
+            'price': response.css('span.buying-priceold-val-number::text').extract_first().strip(),
+            'discount_price': response.css('span.buying-pricenew-val-number::text').extract_first().strip(),
+            'rating': response.css('#rate::text')
+        }
+
+        db.labirint.insert(item)
+        yield item
